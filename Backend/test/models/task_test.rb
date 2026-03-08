24c23909899272e2da428fbb382801bcb4df685e
Fahad <fahadbinqaiser3@gmail.com>
2026-03-08 11:38:59 +0500

    Tests written

--- Backend/test/models/task_test.rb
+++ Backend/test/models/task_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is invalid without a title" do 
+    task = Task.new(title: "", completed: false )
+    assert_not task.valid?
+  end
+  test "is valid with a title and completed" do 
+    task = Task.new(title: "API Integration", completed: false )
+    assert task.valid?
+  end
+  test "is invalid with nil completed" do
+    task = Task.new(title: "API Integration", completed: nil)
+    assert_not task.valid?
+  end
 end
