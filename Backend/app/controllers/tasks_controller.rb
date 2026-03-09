365ce309543aa23ff0cb0b761b343adff1fea5b1
Fahad <fahadbinqaiser3@gmail.com>
2026-03-09 08:32:09 +0500

    Rubocop install and fixed formatting

--- Backend/app/controllers/tasks_controller.rb
+++ Backend/app/controllers/tasks_controller.rb
@@ -1,17 +1,17 @@
 class TasksController < ApplicationController
-    def index 
+    def index
         @tasks = Task.all
         render json: @tasks, status: :ok
     end
-    def create 
+    def create
         @task = Task.new(task_params)
         if @task.save
             render json: @task, status: :created
         else
-            render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity 
+            render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
         end
     end
-    private 
+    private
 
     def task_params
         params.require(:task).permit(:title, :completed)
--- Backend/test/models/task_test.rb
+++ Backend/test/models/task_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  test "is invalid without a title" do 
-    task = Task.new(title: "", completed: false )
+  test "is invalid without a title" do
+    task = Task.new(title: "", completed: false)
     assert_not task.valid?
   end
-  test "is valid with a title and completed" do 
-    task = Task.new(title: "API Integration", completed: false )
+  test "is valid with a title and completed" do
+    task = Task.new(title: "API Integration", completed: false)
     assert task.valid?
   end
   test "is invalid with nil completed" do
