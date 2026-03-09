9a220dc1c61b88080e90ca268cffb490ac3add32
Fahad <fahadbinqaiser3@gmail.com>
2026-03-09 06:53:50 +0500

    Test for created successfully

--- Backend/test/controllers/task_controller_test.rb
+++ Backend/test/controllers/task_controller_test.rb
@@ -5,4 +5,8 @@ class TaskControllerTest < ActionDispatch::IntegrationTest
     get tasks_url, as: :json
     assert_response :success
   end
+  test "POST /tasks return 201" do 
+    post tasks_url, params: {task: {title: "Hello API", completed: true}}, as: :json
+    assert_response :created
+  end
 end
