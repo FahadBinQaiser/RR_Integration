0eccafadfc0c9163b8132be090fc73d430706a13
Fahad <fahadbinqaiser3@gmail.com>
2026-03-27 00:07:55 +0500

    Added data for seed

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Task.create!(title: "hello", completed: true)
+Task.create!(title: "Learning Integration Rails and React only API", completed: false)
